2df8c844271ae0c75b8f53ec1ab52eaeb02c3b43
benzlokzik <121052717+benzlokzik@users.noreply.github.com>
2023-05-04 21:08:12 +0300

    little changes w/ arguments

--- main.py
+++ main.py
@@ -142,14 +142,22 @@ async def pic_decoder(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
         )
     else:
         for i in read_data.decoded:
-            new_image = generators.Generator(i.type, i.data.decode("utf-8")).img
-            new_image = signature.SignatureAdder(new_image)().tobytes()
-            sending_image = BytesIO(new_image)
-            image_file = InputFile(sending_image, "image.jpg")
-            caption = f"Detected {i[0]} code!\nHere's thee value: ```{i[1]}```"
-            await bot.send_photo(
-                chat_id=update.message.chat_id, photo=image_file, caption=caption
-            )
+            caption = f"Detected {i.type} code!\nHere's the value:\n{i.data.decode('utf-8')}"
+            print(caption)
+            await bot.send_message(chat_id=update.message.chat_id, text=caption)
+            # ---
+            # new_image = generators.Generator(i.type, i.data.decode("utf-8")).img
+            # new_image = signature.SignatureAdder(new_image, font_path='ttf/Symbola.ttf')().tobytes()
+            # sending_image = BytesIO(new_image)
+            # image_file = InputFile(sending_image, "image.jpg")
+            # # ---
+            # caption = f"Detected {i.type} code!\nHere's the value:\n{i.data.decode('utf-8')}"
+            # print(caption)
+            # await bot.send_message(chat_id=update.message.chat_id, text=caption)
+            # # ---
+            # await bot.send_photo(
+            #     chat_id=update.message.chat_id, photo=sending_image, caption=caption
+            # )
 
 
 def main():
